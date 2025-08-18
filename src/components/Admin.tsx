d2313381f829874d557e3964a05d48f5cb95e3be
Yemi Ogundairo <62849248+SodiqOgundairo@users.noreply.github.com>
2025-08-18 18:51:54 +0100

    e nv file set

--- src/components/Admin.tsx
+++ src/components/Admin.tsx
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from 'react';
-import { getAllMessages } from '../lib/database';
 import type { Message } from '../lib/database';
 import { FaSpinner } from 'react-icons/fa';
 
