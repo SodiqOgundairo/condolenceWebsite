8dfe7abb4344385e26356dc1c8847ce51b2ec021
Yemi Ogundairo <62849248+SodiqOgundairo@users.noreply.github.com>
2025-08-19 01:33:56 +0100

    supabse implemented

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import Hero from '../components/Hero';
 import MessageForm from '../components/MessageForm';
 import MessageList from '../components/MessageList';
 
@@ -7,10 +6,7 @@ const HomePage: React.FC = () => {
   return (
     <div>
       <div className="container mx-auto px-4">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-12 items-center min-h-screen py-12">
-          <Hero />
           <MessageForm />
-        </div>
       </div>
       <MessageList />
     </div>
